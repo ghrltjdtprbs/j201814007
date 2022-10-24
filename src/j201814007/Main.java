80f33240f61635a166feb2353f24c5f49fd1a5af
Choi, Hyemi <ghrltjdtprbs@gmail.com>
2022-10-24 16:57:46 +0900

    Main.java 수정2

--- src/j201814007/Main.java
+++ src/j201814007/Main.java
@@ -1,9 +1,13 @@
 package j201814007;
 
 public class Main {
-        public static void main(String[] args) {
-        String s = "hello world";
+       
+    static void say(String s) {
         System.out.println(s);
+    }
+    
+    public static void main(String[] args) {
+        say("hello world");
     }
 
 
